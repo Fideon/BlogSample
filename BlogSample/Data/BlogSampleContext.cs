47df0380e3393815f9f53e76178bd366f98185ea
Fidel Salazar <fidelsf91@gmail.com>
2020-12-08 18:46:52 -0600

    Added controller and context

---
+++ BlogSample/Data/BlogSampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogSample.Models;
+
+namespace BlogSample.Data
+{
+    public class BlogSampleContext : DbContext
+    {
+        public BlogSampleContext (DbContextOptions<BlogSampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogSample.Models.BlogPost> BlogPost { get; set; }
+    }
+}
